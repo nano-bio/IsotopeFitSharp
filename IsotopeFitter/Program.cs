f6887ef34f31f4aaea178399e07abc3e6f40414a
enaixtu <misodur@gmail.com>
2017-11-13 16:44:32 +0100

    test of index lookup functions

--- IsotopeFitter/Program.cs
+++ IsotopeFitter/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
+
 using IsotopeFit;
 
 namespace IsotopeFitter
@@ -14,11 +16,35 @@ namespace IsotopeFitter
 
         static void Main(string[] args)
         {
-            Workspace W = new Workspace();  // "testfile.ifd"
+            Workspace W = new Workspace("testfile.ifd");  // 
 
-            W.Dummy();
+            int idx = 0;
+
+            List<double> axisList = W.RawData.MassAxis.ToList();
+            axisList.Sort();
+
+            double[] axis = axisList.ToArray();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            idx = FindLowerLimitIndex(axis, 3000);
+            sw.Stop();
+
+            double elapsed = sw.ElapsedTicks / Stopwatch.Frequency;
+
+            Console.WriteLine(elapsed);
+            Console.WriteLine(idx + " " + axis[idx]);
 
             Console.ReadKey();
+        }
+
+        static int FindLowerLimitIndex(double[] array, double threshold)
+        {
+            int index = Array.BinarySearch(array, threshold);
+
+            if (index >= 0) return index;
+            else return ~index;
         }
     }
 }
