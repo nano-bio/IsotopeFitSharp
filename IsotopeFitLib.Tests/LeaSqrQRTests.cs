57ba0aae5df9e9d1145ae7166713e6bfb18cf367
enaixtu <misodur@gmail.com>
2017-11-21 17:22:57 +0100

    Added test for QR factorization of least squares system using Householder transformations

--- IsotopeFitLib.Tests/LeaSqrQRTests.cs
+++ IsotopeFitLib.Tests/LeaSqrQRTests.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+using System.Diagnostics;
 
 using MathNet.Numerics.LinearAlgebra.Double;
 using MathNet.Numerics.LinearAlgebra;
@@ -19,7 +20,82 @@ namespace IsotopeFitLib.Tests
     public partial class Tests
     {
         [Test]
-        public void LeaSqrQRTest()
+        public void LeaSqrQRHouseholderTest()
+        {
+            //load test data from files
+            string[] Afile = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location) + "\\TestData\\designMatrix.txt");   //TODO: this can fail on Linux because of the backslashes
+            string[] bfile = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location) + "\\TestData\\spectrum.txt");
+
+            List<Vector<double>> Arows = new List<Vector<double>>();
+            List<double> blist = new List<double>();
+
+            foreach (string line in Afile)
+            {
+                List<double> values = new List<double>();
+
+                if (!line.Contains("#") && line != "")
+                {
+                    string[] valuesstr = line.Trim().Split(new char[] { ' ' });
+
+                    foreach (string str in valuesstr)
+                    {
+                        values.Add(Convert.ToDouble(str, new System.Globalization.NumberFormatInfo { NumberDecimalSeparator = "." }));
+                    }
+
+                    Arows.Add(Vector<double>.Build.DenseOfEnumerable(values));
+                }
+            }
+
+            foreach (string line in bfile)
+            {
+                List<double> values = new List<double>();
+
+                if (!line.Contains("#") && line != "")
+                {
+                    blist.Add(Convert.ToDouble(line, new System.Globalization.NumberFormatInfo { NumberDecimalSeparator = "." }));
+                }
+            }
+
+            // original matrix
+            SparseMatrix C = (SparseMatrix)SparseMatrix.Build.SparseOfRowVectors(Arows);
+            Vector<double> d = Vector<double>.Build.DenseOfEnumerable(blist);
+
+            LeastSquaresSystem lss = new LeastSquaresSystem(C, d);
+                        
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            lss = Algorithm.LeaSqrSparseQRHouseholder(lss);
+            s.Stop();
+
+            Console.WriteLine("LeaSqrSparseQRHouseholder " + s.ElapsedMilliseconds);
+
+            // solve the system
+            lss.Solve();
+
+            // solution check
+            string[] xfile = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location) + "\\TestData\\abundances.txt");
+
+            List<double> correctX = new List<double>();
+            
+            foreach (string line in xfile)
+            {
+                if (!line.Contains("#") && line != "")
+                {
+                    correctX.Add(Convert.ToDouble(line.Trim(), new System.Globalization.NumberFormatInfo { NumberDecimalSeparator = "." }));
+                }
+            }
+
+            for (int i = 0; i < correctX.Count; i++)
+            {
+                //Assert.Less(1e-9, Math.Abs(solution[i] - correctX[i]),  "Solution is wrong.");
+                Assert.AreEqual(correctX[i], lss.Solution[i], 1e-9);
+            }
+
+            Assert.Pass("NNLS with sparse QR factorization test passed.");
+        }
+
+        [Test]
+        public void LeaSqrQRGivensTest()
         {
             //load test data from files
             string[] Afile = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location) + "\\TestData\\designMatrix.txt");   //TODO: this can fail on Linux because of the backslashes
@@ -61,7 +137,12 @@ namespace IsotopeFitLib.Tests
 
             LeastSquaresSystem lss = new LeastSquaresSystem(C, d);
 
-            lss = Algorithm.LeaSqrSparseQR(lss);            
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            lss = Algorithm.LeaSqrSparseQRGivens(lss);
+            s.Stop();
+
+            Console.WriteLine("LeaSqrSparseQRGivens " + s.ElapsedMilliseconds);
 
             // solve the system
             lss.Solve();
@@ -70,7 +151,7 @@ namespace IsotopeFitLib.Tests
             string[] xfile = File.ReadAllLines(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location) + "\\TestData\\abundances.txt");
 
             List<double> correctX = new List<double>();
-            
+
             foreach (string line in xfile)
             {
                 if (!line.Contains("#") && line != "")
