58fb5d8216bcc2cf05552829d2554606f60ecbc1
enaixtu <misodur@gmail.com>
2017-12-16 16:29:27 +0100

    rewritten the NNLS to work with the SPQR solver routine

--- IsotopeFitLib/Numerics/LeastSquaresSystem.cs
+++ IsotopeFitLib/Numerics/LeastSquaresSystem.cs
@@ -143,7 +143,7 @@ namespace IsotopeFit
 
                     try
                     {
-                        double[] sol = NNLS2(C, Vector<double>.Build.DenseOfArray(obs)).ToArray();
+                        double[] sol = NNLS3(C, Vector<double>.Build.DenseOfArray(obs)).ToArray();
                         Array.Copy(sol, 0, abd, start, sol.Length);
                     }
                     catch (Exception ex)
@@ -152,63 +152,6 @@ namespace IsotopeFit
                     }                    
                 }
             }
-
-            // --------------------below here it is wrong
-
-            // calculate the block partitioning
-            //int[] blockSizes = new int[nonOverlap.Count - 1];
-
-            //for (int i = 0; i < blockSizes.Length; i++)
-            //{
-            //    blockSizes[i] = nonOverlap[i + 1] - nonOverlap[i];  //TODO: this does not take into account the last block
-            //}
-
-            //for (int i = 0; i < blockSizes.Length; i++)
-            //{
-            //    if (blockSizes[i] != 1)
-            //    {
-            //        int[] indexes = Enumerable.Range(nonOverlap[i] + 1, blockSizes[i] - 1).ToArray();
-            //        int start = nonOverlap[i] + 1;
-            //        int end = start + blockSizes[i] - 1;
-
-            //        int valStart = columnPointers[start];
-            //        int valLength = columnPointers[end] - valStart;
-
-            //        double[] vals = new double[valLength];
-            //        int[] rIdx = new int[valLength];
-            //        int[] cPt = new int[end - start + 1];
-
-            //        Array.Copy(values, valStart, vals, 0, valLength);
-            //        Array.Copy(rowIndices, valStart, rIdx, 0, valLength);
-            //        Array.Copy(columnPointers, start, cPt, 0, end - start + 1);
-
-            //        int ggg = rIdx[0];
-            //        for (int j = 0; j < rIdx.Length; j++)
-            //        {
-            //            rIdx[j] -= ggg;
-            //        }
-
-            //        int hhh = cPt[0];
-            //        for (int j = 0; j < cPt.Length; j++)
-            //        {
-            //            cPt[j] -= hhh;
-            //        }
-
-            //        SparseMatrix C = new SparseMatrix(cPt.Length - 1, cPt.Length - 1);
-            //        {
-            //            C.Values = vals;
-            //            C.RowIndices = rIdx;
-            //            C.ColumnPointers = cPt;
-            //        };
-
-            //        double[] obs = new double[cPt.Length - 1];
-            //        Array.Copy(ObservationVector.ToArray(), start, obs, 0, end - start);
-
-            //        double[] sol = NNLS(C, Vector<double>.Build.DenseOfArray(obs)).ToArray();
-
-            //        Array.Copy(sol, 0, abd, start, sol.Length);
-            //    }
-            //}
 
             Solution = Vector<double>.Build.DenseOfArray(abd);
 
@@ -221,10 +164,11 @@ namespace IsotopeFit
         /// <remarks>
         /// At the moment, only non-negative least squares solver is implemented.
         /// </remarks>
+        [Obsolete]
         public void Solve()
         {
             // at the moment we only need the NNLS method, so no need to add switches for more types
-            Solution = NNLS2(DesignMatrix, ObservationVector);
+            Solution = NNLS3(DesignMatrix, ObservationVector);
 
             // reorder the solution according to the ColumnOrdering information from sparse QR factorization
             Solution = Vector<double>.Build.DenseOfArray(Enumerable.Zip(ColumnOrdering, Solution, (idx, val) => new { idx, val }).OrderBy(v => v.idx).Select(v => v.val).ToArray());
@@ -242,6 +186,371 @@ namespace IsotopeFit
         /// <param name="C">Matrix describing the model.</param>
         /// <param name="d">Vector with observation values.</param>
         /// <returns>MathNet vector with the solution.</returns>
+        private static Vector<double> NNLS3(SparseMatrix C, Vector<double> d)
+        {
+            //TODO: this function can be rewritten to work with sparse matrices and vectors - faster, less ram consumption, nicer
+
+            //TODO: this can be set here as well, we dont really call that many mathnet functions
+            //MathNet.Numerics.Control.UseNativeMKL();
+            //MathNet.Numerics.Control.UseMultiThreading();
+
+            //Declarations and initializations, where necessary.
+            int m = C.RowCount;
+            int n = C.ColumnCount;
+
+            // related to matrix C
+            SparseMatrix CT = (SparseMatrix)C.Transpose();
+            double[][] Carr = new double[C.ColumnCount][];  //TODO: not effective
+
+
+            for (int i = 0; i < C.ColumnCount; i++)
+            {
+                Carr[i] = C.Column(i);
+            }
+
+            SparseMatrix CP;
+            List<double[]> CPList = new List<double[]>();
+            //SparseMatrix CPs;
+            double[][] CPArr = new double[C.ColumnCount][];
+
+            //for (int i = 0; i < C.ColumnCount; i++)
+            //{
+            //    CPArr[i] = new double[C.ColumnCount];
+            //}
+
+            //CP = Matrix<double>.Build.DenseOfColumnArrays(CPArr);
+
+            // solution vectors
+            double[] x = new double[n];
+            //double[] x = new double[n];
+            Vector<double> temp = Vector<double>.Build.Dense(n, 0);
+            double[] z = new double[n];
+
+            // gradient vectors
+            Vector<double> w = Vector<double>.Build.Dense(n, 0);
+            Vector<double> wz = Vector<double>.Build.Dense(n, 0);
+
+            // active and passive sets
+            bool[] P = new bool[n];
+            bool[] A = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                P[i] = false;
+                A[i] = true;
+            }
+
+            // helper variables
+            Vector<double> resid;
+            int outerIter = 0;
+            int innerIter = 0;
+            int iterMax = 3 * n;
+            int zColIndex = 0;
+            double tolx = 10 * MathNet.Numerics.Precision.DoublePrecision * n * C.L1Norm(); // that is how octave does it
+
+            // calculation starts here
+
+            //System.Diagnostics.Debug.WriteLine(d.Count + " " + C.RowCount + " " + C.ColumnCount + " " + x.Count);
+
+            C.Multiply(x, temp.AsArray());  //TODO: may return null
+            resid = d - temp;
+            //w = CT * resid;
+            CT.Multiply(resid.AsArray(), w.AsArray());  //TODO: may return null
+
+            //Pardiso.Initialize(C);
+
+            /* 
+             * Main loop.
+             * First condition checks whether there are still design parameters in the active set.
+             * The second condition first filters out those elements from vector w, that correspond to active design parameters.
+             * Then, those filtered values are compared to convergence tolerance.
+             * In human language: In the end, the active set should be either empty, or the corresponding w elements must be close to zero.
+             */
+            while (A.Any(b => b == true) && w.Where((val, idx) => A[idx] == true).Any(val => val > tolx))
+            {
+                outerIter += 1;
+                Array.Clear(z, 0, z.Length);
+
+                //TODO: check if wz initialization can be implemented using LINQ, something like wz.Where((val, idx) => P[idx] == false).Select(val => val = double.MinValue);
+                for (int i = 0; i < n; i++)
+                {
+                    System.Diagnostics.Debug.Assert(P[i] != A[i]);
+
+                    if (P[i])
+                    {
+                        wz[i] = double.MinValue;
+                    }
+                    else    // that means Z[i] is true
+                    {
+                        wz[i] = w[i];
+                    }
+                }
+
+                // we move the design parameter with the highest gradient (w) to the passive set
+                int t = wz.MaximumIndex();
+                P[t] = true;
+                A[t] = false;
+
+                // find intermediary solution (z)
+                //// count how many active columns we have
+                //int pCols = 0;
+                //for (int i = 0; i < n; i++)
+                //{
+                //    if (P[i]) pCols++;
+                //}
+
+                //// construct the colPointers array
+                //int[] AcolPointers = new int[pCols + 1];
+
+                //pCols = 1;
+                //for (int i = 0; i < n; i++)
+                //{
+                //    if (P[i])
+                //    {
+                //        AcolPointers[pCols] = AcolPointers[pCols - 1] + C.ColumnPointers[i + 1] - C.ColumnPointers[i];
+                //        pCols++;
+                //    }
+                //}
+
+                //// now we can construct the values and row indices
+                //double[] Avalues = new double[AcolPointers.Last()];
+                //int[] ArowIndices = new int[AcolPointers.Last()];
+
+                //pCols = 0;
+                //for (int i = 0; i < n; i++)
+                //{
+                //    if (P[i])
+                //    {
+                //        int sectionLength = AcolPointers[pCols + 1] - AcolPointers[pCols];
+
+                //        Array.Copy(C.Values, C.ColumnPointers[i], Avalues, AcolPointers[pCols], sectionLength);
+                //        Array.Copy(C.RowIndices, C.ColumnPointers[i], ArowIndices, AcolPointers[pCols], sectionLength);
+
+                //        pCols++;
+                //    }
+                //}
+
+                //CP = new SparseMatrix(n, pCols)
+                //{
+                //    Values = Avalues,
+                //    RowIndices = ArowIndices,
+                //    ColumnPointers = AcolPointers
+                //};
+
+                CP = BuildCP(C, P);
+
+                double[] zk = SPQR.Solve(CP, d.ToArray());
+
+                zColIndex = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (P[i])
+                    {
+                        z[i] = zk[zColIndex];
+                        zColIndex++;
+                    }
+                }
+
+                //inner loop - check if any regression coefficient has turned negative
+                while (z.Where((val, idx) => P[idx] == true).Any(val => val <= 0))
+                {
+                    innerIter++;
+                    if (innerIter > iterMax)
+                    {
+                        throw new Exception("max pocet iteracii");
+                    }
+
+                    List<double> Q = new List<double>();
+
+                    zColIndex = 0;
+
+                    // get all negative regression coefficients
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (P[i] && z[i] <= 0)  // 
+                        {
+                            //if (z[zColIndex] <= 0) //
+                            //{
+                            //TODO: maybe it does not have to be a list, because we iterate through the inner loop
+                            double derp = x[i] / (x[i] - z[i]);     //zColIndex
+                            Q.Add(derp);
+                            //}
+
+                            //zColIndex++;
+                        }
+                    }
+
+                    // find the optimal alpha to make correction with
+                    double alpha = Q.Min();
+                    //zColIndex = 0;
+
+                    // find the solution that does not violate the constraints using that alpha
+                    for (int i = 0; i < n; i++)
+                    {
+                        //if (P[i])
+                        //{
+                        x[i] += alpha * (z[i] - x[i]); //zColIndex
+                                                       //zColIndex++;
+                                                       //}
+                                                       //else
+                                                       //{
+                                                       //    x[i] -= alpha * x[i];
+                                                       //}
+
+                        // update P and Z accordingly
+                        if (Math.Abs(x[i]) < tolx && P[i])
+                        {
+                            A[i] = true;
+                        }
+
+                        P[i] = !A[i];
+                    }
+
+                    // recalculate for z
+                    //CPList.Clear();
+                    //Array.Copy(Carr, CPArr, Carr.Length);
+
+                    //for (int i = 0; i < n; i++)
+                    //{
+                    //    if (P[i])
+                    //    {
+                    //        CPList.Add(Carr[i]);
+                    //    }
+
+                    //    //if (!P[i])
+                    //    //{
+                    //    //    for (int j = 0; j < CPArr[i].Length; j++)
+                    //    //    {
+                    //    //        CPArr[i][j] = 0;
+                    //    //    }
+                    //    //}
+                    //}
+
+                    //CP = Matrix<double>.Build.DenseOfColumnArrays(CPList.ToArray());
+                    //CP = Matrix<double>.Build.DenseOfColumnArrays(CPArr);
+                    //CP = Matrix<double>.Build.SparseOfColumnArrays(CPList.ToArray());
+                    //zk = CP.QR(QRMethod.Thin).Solve(d).ToArray();
+                    //zk = Pardiso.Solve(CP, d.ToArray(), P);
+                    //z = Vector<double>.Build.Dense(ggg);
+
+                    CP = BuildCP(C, P);
+
+                    zk = SPQR.Solve(CP, d.ToArray());
+
+                    Array.Clear(z, 0, z.Length);
+                    zColIndex = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (P[i])  // && z[i] <= 0
+                        {
+                            z[i] = zk[zColIndex];   //
+                            zColIndex++;
+                        }
+                    }
+
+                    //for (int i = 0; i < z.Count; i++)
+                    //{
+                    //    if (!P[i]) z[i] = 0;
+                    //}
+                }
+
+                // calculate gradient
+                //zColIndex = 0;
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //if (P[i])
+                //{
+                //x[i] = z[i];    //zColIndex
+                //zColIndex++;
+                //}
+                //}
+                Array.Copy(z, x, z.Length);
+                //x = Vector<double>.Build.DenseOfVector(z);
+
+                //resid = d - C * x;
+                //w = CT * resid;
+
+                try
+                {
+                    C.Multiply(x, temp.AsArray());  //TODO: may return null
+                    resid = d - temp;
+                    //w = CT * resid;
+                    CT.Multiply(resid.AsArray(), w.AsArray());  //TODO: may return null
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return Vector<double>.Build.DenseOfArray(x);
+        }
+
+        private static SparseMatrix BuildCP(SparseMatrix C, bool[] P)
+        {
+            // count how many active columns we have
+            int pCols = 0;
+            for (int i = 0; i < C.RowCount; i++)
+            {
+                if (P[i]) pCols++;
+            }
+
+            // construct the colPointers array
+            int[] AcolPointers = new int[pCols + 1];
+
+            pCols = 1;
+            for (int i = 0; i < C.RowCount; i++)
+            {
+                if (P[i])
+                {
+                    AcolPointers[pCols] = AcolPointers[pCols - 1] + C.ColumnPointers[i + 1] - C.ColumnPointers[i];
+                    pCols++;
+                }
+            }
+
+            // now we can construct the values and row indices
+            double[] Avalues = new double[AcolPointers.Last()];
+            int[] ArowIndices = new int[AcolPointers.Last()];
+
+            pCols = 0;
+            for (int i = 0; i < C.RowCount; i++)
+            {
+                if (P[i])
+                {
+                    int sectionLength = AcolPointers[pCols + 1] - AcolPointers[pCols];
+
+                    Array.Copy(C.Values, C.ColumnPointers[i], Avalues, AcolPointers[pCols], sectionLength);
+                    Array.Copy(C.RowIndices, C.ColumnPointers[i], ArowIndices, AcolPointers[pCols], sectionLength);
+
+                    pCols++;
+                }
+            }
+
+            SparseMatrix CP = new SparseMatrix(C.RowCount, pCols)
+            {
+                Values = Avalues,
+                RowIndices = ArowIndices,
+                ColumnPointers = AcolPointers
+            };
+
+            return CP;
+        }
+
+        /// <summary>
+        /// Calculate a non-negative least squares solution of C * x = d
+        /// </summary>
+        /// <remarks>
+        /// The algorithm was originally published in:
+        /// Lawson, Hanson, Solving Least Squares Problems, 1987, ISBN 978-0-89871-356-5, p. 160, Chapter 23.3
+        /// Another description of the same algorithm, but easier to understand has been published in:
+        /// Bro, De Jong, 1997, A fast non-negativity-constrained least squares algorithm, J. Chemom. 11, pp. 393-401
+        /// </remarks>
+        /// <param name="C">Matrix describing the model.</param>
+        /// <param name="d">Vector with observation values.</param>
+        /// <returns>MathNet vector with the solution.</returns>
+        [Obsolete]
         private static Vector<double> NNLS2(SparseMatrix C, Vector<double> d)
         {
             //TODO: this function can be rewritten to work with sparse matrices and vectors - faster, less ram consumption, nicer
@@ -569,6 +878,7 @@ namespace IsotopeFit
         /// <param name="C">Matrix describing the model.</param>
         /// <param name="d">Vector with observation values.</param>
         /// <returns>MathNet vector with the solution.</returns>
+        [Obsolete]
         private static Vector<double> NNLS(SparseMatrix C, Vector<double> d)
         {
             //TODO: this function can be rewritten to work with sparse matrices and vectors - faster, less ram consumption, nicer
