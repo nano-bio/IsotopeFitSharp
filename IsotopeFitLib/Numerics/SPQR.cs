4efab4e25e232e0f62fb4153b637f6d9a09c65f3
enaixtu <misodur@gmail.com>
2017-12-16 16:28:24 +0100

    added functions that interface to the SPQR sparse equation system solver

--- IsotopeFitLib/Numerics/SPQR.cs
+++ IsotopeFitLib/Numerics/SPQR.cs
@@ -23,10 +23,28 @@ namespace IsotopeFit
             [In] int rows, [In] int cols, [In] int nzCount, [In] double[] vals, [In] long[] rowInd, [In] long[] colPtr,
             out int Rrows, out int Rcols, out int RnzCount, out IntPtr Rvals, out IntPtr RrowInd, out IntPtr RcolPtr, out IntPtr ROrdering);
 
-        public static SparseMatrix Calculate(SparseMatrix A)
+        [DllImport("SPQR.dll")]
+        private static extern void SparseSolveDispose(IntPtr[] handle);
+
+        [DllImport("SPQR.dll")]
+        private static extern int SparseSolve(
+            IntPtr[] handle,
+            [In] int rows, [In] int cols, [In] int nzCount, [In] double[] vals, [In] long[] rowInd, [In] long[] colPtr,
+            double[] bVals,
+            out IntPtr xVals);
+
+        /// <summary>
+        /// Calculates the QR factorization of a sparse matrix.
+        /// </summary>
+        /// <remarks>
+        /// Discards the calculated values of the Q matrix, in order to be faster and more memory efficient.
+        /// </remarks>
+        /// <param name="A">Sparse matrix to be factorized.</param>
+        /// <returns>Upper triangular factor R, in compressed sparse column format.</returns>
+        public static SparseMatrix QR(SparseMatrix A)
         {
             IntPtr[] handles = new IntPtr[3];
-            int ordering = 5;   // 5 - AMD mimimum degree, 6 - metis mindeg, 
+            int ordering = 5;
             double tolerance = 1e-9;
 
             int rows = A.RowCount;
@@ -36,7 +54,7 @@ namespace IsotopeFit
             long[] rowIndices = Array.ConvertAll<int, long>(A.RowIndices, a => a);
             long[] colPointers = Array.ConvertAll<int, long>(A.ColumnPointers, a => a);
 
-            int handle = SparseQR(handles,
+            int status = SparseQR(handles,
                 ordering, tolerance,
                 rows, cols, nzCount, values, rowIndices, colPointers,
                 out int Rrows, out int Rcols, out int RnzCount, out IntPtr Rvals, out IntPtr RrowInd, out IntPtr RcolPtr, out IntPtr RorderingPtr);
@@ -62,6 +80,33 @@ namespace IsotopeFit
             };
 
             return R;
+        }
+
+        /// <summary>
+        /// Solves the sparse linear equation system A * x = b.
+        /// </summary>
+        /// <param name="A">Sparse matrix of coefficients.</param>
+        /// <param name="b">Array of observation values.</param>
+        /// <returns>Array containing the solution of the linear equation system.</returns>
+        public static double[] Solve(SparseMatrix A, double[] b)
+        {
+            IntPtr[] handles = new IntPtr[4];
+
+            int rows = A.RowCount;
+            int cols = A.ColumnCount;
+            int nzCount = A.NonZerosCount;
+            double[] values = A.Values;
+            long[] rowIndices = Array.ConvertAll<int, long>(A.RowIndices, a => a);
+            long[] colPointers = Array.ConvertAll<int, long>(A.ColumnPointers, a => a);
+
+            int status = SparseSolve(handles, rows, cols, nzCount, values, rowIndices, colPointers, b, out IntPtr xVals);
+
+            double[] x = new double[rows];
+            Marshal.Copy(xVals, x, 0, status);
+
+            SparseSolveDispose(handles);
+
+            return x;
         }
     }
 }
