e2d826fd569fa383a6d9bc92a5bc222feb5f267d
Michal Durian <misodur@gmail.com>
2018-01-17 19:01:47 +0100

    added a static fit function for python GUI

---
+++ IsotopeFitLib/PyGUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsotopeFit
+{
+    public static class PyGUI
+    {
+        public static void Fit(
+            double[] massAxis, double[] signalAxis,    // spectral data
+            Dictionary<string, IFData.Cluster> clusters, List<string> clusterIDList,    // cluster data
+            int resolutionFitType, double[] resolutionBreaks, double[] resolutionCoeffs,    // resolution fit data
+            double[] peakShapeBreaks, double[] peakShapeCoeffs,    // peak shape data
+            double dmSearchRange, double dmFwhmRange    // needed for design matrix calculation
+            )
+        {
+            Workspace W = new Workspace();
+
+            W.SpectralData.MassAxis = massAxis;
+            W.SpectralData.SignalAxisCrop = signalAxis;
+            
+            // TODO: fill the cluster dictionary
+
+            W.BuildDesignMatrix(dmSearchRange, dmFwhmRange);  // TODO: make the parameters adjustable
+
+            W.FitAbundances();
+
+            // TODO: extract the abundance (and errors maybe, anything else?) and put it to some dictionary, return that
+        }
+    }
+}
