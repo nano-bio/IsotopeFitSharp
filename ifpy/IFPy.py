50e508b2302156bf50b60405ea6e49556d8b2f7e
Michal Durian <misodur@gmail.com>
2018-02-24 13:52:29 +0100

    added more arguments to the fit function, comments

--- ifpy/IFPy.py
+++ ifpy/IFPy.py
@@ -3,9 +3,30 @@ from System.Collections.Specialized import OrderedDictionary
 from IsotopeFit import PyGUI as pg
 from IsotopeFit import IFData
 
-def fit(massAxis, signalAxis, peakDataDict):
+def fit(massAxis, signalAxis,
+		peakDataDict,
+		resolutionPPBreaks, resolutionCoeffs,
+		peakShapeBreaks, peakShapeCoeffs,
+		dmSearchRange, dmFwhmRange):
+	"""Function for fitting the spectrum from Python GUI.
+
+	Args:
+		massAxis			(list): Corrected mass axis.
+		signalAxis			(list): Corrected signal axis.
+		peakDataDict		(dict of 2d lists): Structure containing the isotope data for each cluster, with the following structure: { "cluster1":[ [mass1, abundance1], [mass2, abundance2], ...], "cluster2":[...]}
+		resolutionPPBreaks	(list): If the resolution fit was a partial polynomial, this contains the PP breaks. Otherwise None.
+		resolutionCoeffs	(2d list): Resolution fit coefficients for each partial polynomial. If the root list contains only a single child list, a polynomial interpolation is assumed.
+		peakShapeBreaks		(list): Breaks of the line shape fit partial polynomials.
+		peakShapeCoeffs		(2d list): Coefficients of the line shape partial polynomials.
+		dmSearchRange		(float64): Search range for the design matrix build function.
+		dmFwhmRange			(float64): FWHM range for the design matrix build function.
+
+	Returns:
+		dict: Dictionary with cluster IDs as keys, containing small 2-key dictionaries with "abundance" and "abundanceError" data for each cluster.
+	
+	"""
 	# create and fill the .NET dictionary
-	dotNetDict = OrderedDictionary()
+	clusters = OrderedDictionary()
 
 	for key in peakDataDict.keys():
 		cluster = IFData.Cluster();
@@ -13,18 +34,18 @@ def fit(massAxis, signalAxis, peakDataDict):
 		peakData = IFData.Cluster.IsotopeData(peakDataDict[key]);
 		cluster.PeakData = peakData
 
-		dotNetDict.Add(key, cluster)
+		clusters.Add(key, cluster)
 
 	# call the .NET fit function
-	pg.TestFit2(dotNetDict)
+	pg.Fit(massAxis, signalAxis, clusters, resolutionPPBreaks, resolutionCoeffs, peakShapeBreaks, peakShapeCoeffs, dmSearchRange, dmFwhmRange)
 
 	# put the results back to a python dictionary
 	abundanceDict = {}
 
 	for key in peakDataDict.keys():
 		abundanceDict[key] = {}
-		abundanceDict[key]["abundance"] = dotNetDict[key].Abundance
-		abundanceDict[key]["abundanceError"] = dotNetDict[key].AbundanceError
+		abundanceDict[key]["abundance"] = clusters[key].Abundance
+		abundanceDict[key]["abundanceError"] = clusters[key].AbundanceError
 
 	return abundanceDict
 
